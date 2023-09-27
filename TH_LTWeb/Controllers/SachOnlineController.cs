bb27aca007cbbeca11a5454885ac5f559a4bc77b
nguyen xuan tien <xuantien24689@gmail.com>
2023-09-27 21:11:38 +0700

    lab02

---
+++ TH_LTWeb/Controllers/SachOnlineController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TH_LTWeb.Models;
+
+namespace TH_LTWeb.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        // GET: SachOnline
+        dbSachOnlineDataContext data = new dbSachOnlineDataContext();
+        private List<Sach> LaySachMoi(int count)
+        {
+            return data.Saches.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
+        }
+        public ActionResult Index()
+        {
+            var listSachMoi = LaySachMoi(6);
+            return View(listSachMoi);
+        }
+        public ActionResult ChuDePartial()
+        {
+            var listChuDe = from cd in data.CHUDEs select cd;
+            return PartialView(listChuDe);
+        }
+    }
+}
---
+++ TH_LTWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TH_LTWeb.MvcApplication" Language="C#" %>
